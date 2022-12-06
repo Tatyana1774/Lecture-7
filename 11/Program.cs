aa2e3ffb325e7a49efe59dd6df6ea2fce67b0d93
Tatyana <tatyana.volkova1984@mail.ru>
2022-12-06 21:34:33 +0500

    Фибоначчи

---
+++ 11/Program.cs
@@ -0,0 +1,36 @@
+﻿// Фибоначчи
+decimal fRec = 0;
+decimal fIte = 0;
+decimal FibonacciRecursion(int n)
+{
+    fRec++;
+    return n == 0 || n == 1 ? 1 : FibonacciRecursion(n - 1) + FibonacciRecursion(n - 2);
+}
+decimal FibonacciIteration(int n)
+{
+    fIte++;
+    decimal result = 1;
+    decimal f0 = 1;
+    decimal f1 = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        f0 = f1;
+        f1 = result;
+        fIte++;
+    }
+    return result;
+}
+Console.ReadLine();
+for (int n = 10; n < 40; n++)
+{
+    Console.WriteLine($"FibonacciIteration({n}) = {FibonacciIteration(n)} fIte = {fIte.ToString("### ### ### ###"), -15}");
+    fIte = 0;
+}
+Console.WriteLine();
+Console.ReadLine();
+for (int n = 10; n < 40; n++)
+{
+    Console.WriteLine($"FibonacciRecursion({n}) = {FibonacciRecursion(n)} fRec = {fRec.ToString("## ### ### ###"), -15}");
+    fRec = 0;
+}
+Console.ReadLine();
