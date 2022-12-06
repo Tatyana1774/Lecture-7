d38d85af1893eeb79fe329792545ec6436232e9f
Tatyana <tatyana.volkova1984@mail.ru>
2022-12-06 20:52:50 +0500

    Обход директории. Что в папке? Папка с папками) ссылка неправильная

---
+++ 8/Program.cs
@@ -0,0 +1,30 @@
+﻿//Обход директории. Что в папке? Папка с папками)
+string path = "C:/Users/ACER/Desktop/lec7/3";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
+
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent} {files[i].Name}");
+    }
+}
+string path = @"C:/Users/ACER/Desktop/lec7/3";
+CatalogInfo(path);
