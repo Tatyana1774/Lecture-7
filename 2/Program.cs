a153607e9c931054154f56919b1cf502dd011e79
Tatyana <tatyana.volkova1984@mail.ru>
2022-12-06 19:49:24 +0500

    Собрать строку от a до b, a>=b

---
+++ 2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Собрать строку от a до b, a>=b
+string NumberFor (int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i >= b; i--)
+    {
+        result += $"{i}";
+    }
+    return result;
+}
+
+string NumberRec (int a, int b)
+{
+    if (a <= b)
+    {
+        return NumberRec(a + 1, b) + $"{a}";
+    }
+    else
+    {
+        return String.Empty;
+    }
+}
+
+Console.WriteLine (NumberFor(1, 10));
+Console.WriteLine (NumberRec(1, 10));
