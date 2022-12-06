03eaae8fdf1cbed081dd33f6ed1dd2eb685043b6
Tatyana <tatyana.volkova1984@mail.ru>
2022-12-06 20:29:39 +0500

    Сбор всех слов с помощью void метода

---
+++ 7/Program.cs
@@ -0,0 +1,17 @@
+﻿// Перебор слов
+int n = 1;
+void FindWords(string alphabet, char[] word, int Length = 0)
+{
+    if (Length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}");
+        return;
+    }
+
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[Length] = alphabet[i];
+        FindWords(alphabet, word, Length + 1);
+    }
+}
+FindWords("аисв", new char[4]);
